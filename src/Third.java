bb35ada511378e95aa8c820b710e5ad597831da9
User <230519@astanait.edu.kz>
2024-04-23 15:54:47 +0500

    1

---
+++ src/Third.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Third {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> a = new ArrayList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (x == 1){
+                int y = sc.nextInt();
+                a.addFirst(y);
+            }
+            if (x == 2){
+                int y = sc.nextInt();
+                a.add(y);
+            }
+            if (x == 3){
+                System.out.println(a.get(0));
+            }
+            if (x == 4){
+                System.out.println(a.get(a.size()-1));
+            }
+        }
+    }
+}
