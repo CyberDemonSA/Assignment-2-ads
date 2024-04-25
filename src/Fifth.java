202e6f09e7e1651f9fe400ec9c7c86a66f7575fc
User <230519@astanait.edu.kz>
2024-04-25 18:05:15 +0500

    1

--- src/Fifth.java
+++ src/Fifth.java
@@ -1,3 +1,36 @@
-public class Fifth {
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
 
+public class Fifth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            b.add(sc.nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            a.add(sc.nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a.isEmpty() || b.isEmpty()) {break;}
+                if (Objects.equals(a.getFirst(), b.getFirst())) {
+                    System.out.println(a);
+                    System.out.println(b);
+                    a.removeFirst();
+                    b.removeFirst();
+                }
+                else {
+                    System.out.println(a);
+                    System.out.println(b);
+                    b.add(b.getFirst());
+                    b.removeFirst();
+                }
+            }
+        }
+        System.out.println(b.size());
+    }
 }
